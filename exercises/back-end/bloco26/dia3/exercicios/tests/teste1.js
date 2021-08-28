e01421d5baf2dabc1c7051eb1bfcc90c8198bfe2
Felipe Muller <felipeavmuller@gmail.com>
2021-08-28 18:36:29 -0300

    Exercicio 2 utilizando tdd - implementacao CHAI e MOCHA

--- exercises/back-end/bloco26/dia3/exercicios/tests/teste1.js
+++ exercises/back-end/bloco26/dia3/exercicios/tests/teste1.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const verificaNumero = require('../verificaNumero');
 
-describe('Executa a função verificaNumero', () => {
+describe('1. Executa a função verificaNumero', () => {
   describe('Quando o número é maior que 0', () => {
     const result = verificaNumero(10);
     it('Retorna uma string', () => {
@@ -31,4 +31,14 @@ describe('Executa a função verificaNumero', () => {
       expect(result).to.be.equal('neutro');
     });
   });
+
+  describe('Quando o valor informado não é um número', () => {
+    const result = verificaNumero('teste');
+    it('Retorna uma string', () => {
+      expect(result).to.be.a('string');
+    });
+    it('Retorna a mensagem "o valor deve ser um número"', () => {
+      expect(result).to.be.equal('o valor deve ser um número');
+    })
+  })
 });
