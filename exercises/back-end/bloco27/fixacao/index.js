771a5706e4a173c9bbcd7dc99a563df74526f835
Felipe Muller <felipeavmuller@gmail.com>
2021-09-04 18:21:52 -0300

    Criando o post - exercicio 4

--- exercises/back-end/bloco27/fixacao/index.js
+++ exercises/back-end/bloco27/fixacao/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const Book = require('./models/Book');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/books', async (req, res) => {
   const { id } = req.query;
@@ -13,19 +14,29 @@ app.get('/books', async (req, res) => {
 
    const books = (id) ? await Book.getByAuthorId(id)
     : await Book.getAll();
-    
+
   if (!books || books.length === 0) return res.json({message: 'Nenhum livro encontrado'})
   res.status(200).json(books);
 });
 
-// app.get('/books/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const book = await Book.getByAuthorId(id);
-//   console.log(book);
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.getById(id);
+  if (!book) return res.status(400).json({message: 'Book not found'});
+  res.status(200).json(book);
+})
 
-//   if (!book) return res.status(400).json({message: 'ID inexistente'});
-//   res.status(200).json(book);
-// })
+app.post('/books', async (req, res) => {
+  const { title, author_id } = req.body;
+  
+  if (!await Book.isValid(title, author_id)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+  
+  await Book.create(title, author_id);
+  
+  res.status(201).json({ message: 'Livro criado com sucesso! '});
+  });
 
 const PORT = process.env.PORT || 3000;
 
--- exercises/back-end/bloco27/fixacao/models/Book.js
+++ exercises/back-end/bloco27/fixacao/models/Book.js
@@ -23,7 +23,35 @@ const getByAuthorId = async (id) => {
   }))
 };
 
+const getById = async (id) => {
+  const [books] = await connection.execute(
+    'SELECT * FROM model_example.books WHERE id=?',
+    [id],
+  );
+
+  return books.map(({id, title, author_id}) => ({
+    id,
+    title,
+    authorId: author_id,
+  }))[0];
+}
+
+const isValid = async (title, authorId) => {
+  if (!title || title.length < 3) return false;
+  if (!authorId || typeof authorId !== 'number') return false;
+
+  return true;
+};
+
+const create = async (title, authorId) => await connection.execute(
+'INSERT INTO model_example.books (title, author_id) VALUES (?,?)',
+[title, authorId],
+);
+
 module.exports = {
   getAll,
   getByAuthorId,
+  getById,
+  isValid,
+  create,
 };
